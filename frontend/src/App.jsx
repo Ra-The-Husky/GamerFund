bbc0eefa7545f475222c4b1c2431f15d7a2b8ee1
Ra Lin <rasheedlindsey@live.com>
2024-04-14 17:32:33 -0400

    Added Signup reducer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import * as sessionActions from './store/session';
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage/SignupFormPage";
+import * as sessionActions from "./store/session";
 
 function Layout() {
   const dispatch = useDispatch();
@@ -10,15 +11,11 @@ function Layout() {
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => {
-      setIsLoaded(true)
+      setIsLoaded(true);
     });
   }, [dispatch]);
 
-  return (
-    <>
-      {isLoaded && <Outlet />}
-    </>
-  );
+  return <>{isLoaded && <Outlet />}</>;
 }
 
 const router = createBrowserRouter([
@@ -26,15 +23,23 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        path: '/',
-        element: <h1>Welcome!</h1>
+        path: "/",
+        element: <h1>GamerFund Coming Soon!</h1>,
       },
       {
-        path: '/login',
-        element: <LoginFormPage />
-      }
-    ]
-  }
+        path: "/login",
+        element: <LoginFormPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupFormPage />,
+      },
+      {
+        path: "*",
+        element: <h1>Not Found, My Friend</h1>,
+      },
+    ],
+  },
 ]);
 
 function App() {
