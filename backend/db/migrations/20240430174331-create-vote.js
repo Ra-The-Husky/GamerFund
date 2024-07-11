6e7ee7431a59b99568867eda401433eb8157601a
Ra Lin <rasheedlindsey@live.com>
2024-07-11 13:19:13 -0400

    Vote migration update -- Render Deployment

--- backend/db/migrations/20240430174331-create-vote.js
+++ backend/db/migrations/20240430174331-create-vote.js
@@ -1,4 +1,9 @@
 "use strict";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -33,7 +38,7 @@ module.exports = {
       },
     });
   },
-  
+
   async down(queryInterface, Sequelize) {
     options.tableName = "Votes";
     return queryInterface.dropTable(options);
