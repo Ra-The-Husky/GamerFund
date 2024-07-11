8e5b83877098be3cef81cf244663722e59a6c30c
Ra Lin <rasheedlindsey@live.com>
2024-07-11 13:24:41 -0400

    Vote Migration Update #2 -- Render Deploy

--- backend/db/migrations/20240430174331-create-vote.js
+++ backend/db/migrations/20240430174331-create-vote.js
@@ -7,36 +7,42 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Votes", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "Votes",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+        },
+        discussionId: {
+          type: Sequelize.INTEGER,
+        },
+        liked: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false,
+        },
+        disliked: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      userId: {
-        type: Sequelize.INTEGER,
-      },
-      discussionId: {
-        type: Sequelize.INTEGER,
-      },
-      liked: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-      },
-      disliked: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
+      options
+    );
   },
 
   async down(queryInterface, Sequelize) {
